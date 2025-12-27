c277eb1475f0eeeaf37195f4d521dbf6cf6905aa
CLADForge <yunusebusiness@gmail.com>
2025-12-27 12:26:04 +0300

    feat: ürün kartı (ProductCard) bileşeni ana yapısı oluşturuldu

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProductCard() {
+  return (
+    <div>
+      {/* Buraya ürün kartı tasarımı gelecek */}
+      <p>Product Card</p>
+    </div>
+  );
+}
